479ee14b2368d1f0732cb404513b9e5b7eb24fe5
Stuart Telford <stuart_telford@hotmail.com>
2022-11-22 12:10:50 +0000

    Changed method of initialising variables. Added Date to comments.

--- character_frequency.py
+++ character_frequency.py
@@ -1,35 +1,13 @@
 # Count vowels in text
+# 20221122
 
 source_txt_original = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet elit gravida, tempor tortor eget, tincidunt mauris. Nunc aliquet quam nec metus cursus lacinia. Cras tempus eget nibh et dictum. Donec at volutpat tortor, sit amet gravida risus. Pellentesque facilisis in quam at faucibus. In hac habitasse platea dictumst. Ut varius dapibus pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat mi maximus, gravida purus at, maximus libero. Aenean a pharetra orci. Donec dignissim ante quis elit dapibus, eu elementum augue ullamcorper. Proin tempor enim eget nunc ultricies, eu sagittis quam iaculis. Nam commodo porta mi, non lobortis leo tincidunt sed. Nunc sit amet placerat justo, non porta justo. Maecenas ultrices, justo sed vulputate lacinia, lorem tellus lobortis felis, ut lobortis arcu nulla condimentum dolor. Proin cursus justo at ullamcorper ultrices.'
 
 source_txt = source_txt_original.lower()
 
-a = 0
-b = 0
-c = 0
-d = 0
-e = 0
-f = 0
-g = 0
-h = 0
-i = 0
-j = 0
-k = 0
-l = 0
-m = 0
-n = 0
-o = 0
-p = 0
-q = 0
-r = 0
-s = 0
-t = 0
-u = 0
-v = 0
-w = 0
-x = 0
-y = 0
-z = 0
+# Set all variables = 0
+a = e = i = o = u = 0
+#a = b = c = d = e = f = g = h = i = j = k = l = m = n = o = p = q = r = s = t = u = v = w = x = y = z = 0
 
 # Loop through text checking if each character is a vowel and keeping count.
 for tst_char in range(len(source_txt)):
